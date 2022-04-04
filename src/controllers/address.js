6cc2e2807383abcb5b012cbf9286fbafa19c248c
Ayush Gupta <ayush.g@sudoboat.com>
2022-04-04 11:44:51 +0530

    update route added for address

--- src/controllers/address.js
+++ src/controllers/address.js
@@ -74,8 +74,151 @@ router.get("/:userId", (req, res) => {
                         * API for updating address of a user.
                         */
 router.put("/:userId", (req, res) => {
-  
-})
+
+  if (req.body.length === 1) {
+    const { address_id, state, district, taluk, village, city, pincode, postoffice,
+      isPermanent, isCurrent } = req.body[0];
+
+    // console.log([address_id, state, district, taluk, village, city,
+    //              pincode, postoffice, isPermanent, isCurrent]);
+    const requestedId = req.params.userId;
+
+    const Query = `UPDATE ${SCHEMA_NAME}.${TABLE_NAMES.address}
+                   SET state = $1,
+                       district = $2,
+                       taluk = $3,
+                       village = $4,
+                       city = $5,
+                       pincode = $6,
+                       postoffice = $7,
+                       isPermanent = $8,
+                       isCurrent = $9
+                  WHERE user_id = $10 `;
+    pool.query(
+      Query,
+      [state, district, taluk, village, city, pincode, postoffice, isPermanent, isCurrent, requestedId],
+      (error, result) => {
+        res.json({ response : "User updated."});
+      }
+    );
+  }
+
+   else {
+     console.log("comes here ------------");
+     if ( req.body[0].isPermanent && req.body[0].isCurrent ||
+         req.body[1].isPermanent && req.body[1].isCurrent ) {
+
+       var ind;
+       if (req.body[0].isPermanent) {
+         ind = 0;
+       }
+       else {
+         ind = 1;
+       }
+
+       const { address_id, state, district, taluk, village, city, pincode, postoffice,
+         isPermanent, isCurrent } = req.body[ind];
+         console.log([address_id, state, district, taluk, village, city, pincode, postoffice,
+           isPermanent, isCurrent]);
+
+         const requestedId = req.params.userId;
+
+         const Query1 = `UPDATE ${SCHEMA_NAME}.${TABLE_NAMES.address}
+                        SET state = $1,
+                            district = $2,
+                            taluk = $3,
+                            village = $4,
+                            city = $5,
+                            pincode = $6,
+                            postoffice = $7,
+                            isPermanent = $8,
+                            isCurrent = $9
+                       WHERE address_id = $10`;
+         pool.query(
+           Query1,
+           [state, district, taluk, village, city, pincode, postoffice, isPermanent, isCurrent, address_id],
+           (error, result) => {
+             if (error) console.log(error);
+           }
+         );
+
+        const Query2 = `DELETE FROM ${SCHEMA_NAME}.${TABLE_NAMES.address}
+                         WHERE user_id = $1 AND address_id != $2`;
+        pool.query(
+          Query2,
+          [requestedId, address_id],
+          (error, result) => {
+            if (error) console.log(error);
+          }
+        );
+
+        res.json({ response : "User updated!" });
+     }
+
+     else if (req.body[0].isPermanent != req.body[0].isCurrent &&
+              req.body[1].isPermanent != req.body[1].isCurrent) {
+
+          if (req.body[0].isPermanent != req.body[0].isCurrent) {
+
+            const { address_id, state, district, taluk, village, city, pincode, postoffice,
+                    isPermanent, isCurrent } = req.body[0];
+            const requestedId = req.params.userId;
+            const Query1 = `UPDATE ${SCHEMA_NAME}.${TABLE_NAMES.address}
+                                   SET state = $1,
+                                       district = $2,
+                                       taluk = $3,
+                                       village = $4,
+                                       city = $5,
+                                       pincode = $6,
+                                       postoffice = $7,
+                                       isPermanent = $8,
+                                       isCurrent = $9
+                                  WHERE address_id = $10`;
+              pool.query(
+                  Query1,
+                  [state, district, taluk, village, city, pincode, postoffice, isPermanent, isCurrent, address_id],
+                  (error, result) => {
+                      if (error) console.log(error);
+                  }
+              );
+          }
+          if (req.body[1].isPermanent != req.body[1].isCurrent) {
+
+            const { address_id, state, district, taluk, village, city, pincode, postoffice,
+                    isPermanent, isCurrent } = req.body[1];
+            const requestedId = req.params.userId;
+            const Query1 = `UPDATE ${SCHEMA_NAME}.${TABLE_NAMES.address}
+                                   SET state = $1,
+                                       district = $2,
+                                       taluk = $3,
+                                       village = $4,
+                                       city = $5,
+                                       pincode = $6,
+                                       postoffice = $7,
+                                       isPermanent = $8,
+                                       isCurrent = $9
+                                  WHERE address_id = $10`;
+              pool.query(
+                  Query1,
+                  [state, district, taluk, village, city, pincode, postoffice, isPermanent, isCurrent, address_id],
+                  (error, result) => {
+                      if (error) console.log(error);
+                  }
+              );
+          }
+
+          res.json({ response : "User updated!" });
+
+     }
+
+   }
+});
+
+
+
+
+
+
 
 
 
